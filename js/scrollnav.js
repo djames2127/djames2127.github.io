68da45027c01a1f2edfb3478640360c3266d9f7b
Daniel James <djames@nubrink.com>
2015-07-25 12:18:42 +0800

    2015 Rocket & Space Theme

--- js/scrollnav.js
+++ js/scrollnav.js
@@ -25,3 +25,43 @@ $(function() {
         }
     });
 });
+
+
+
+//Bootstrap Multiple Expand/Collapse
+$(function () {			
+                    $('a[data-toggle="collapse"]').on('click',function(){
+				
+				var objectID=$(this).attr('href');
+				
+				if($(objectID).hasClass('in'))
+				{
+                                    $(objectID).collapse('hide');
+				}
+				
+				else{
+                                    $(objectID).collapse('show');
+				}
+                    });
+                    
+                    
+                    $('#expandAll').on('click',function(){
+                        
+                        $('a[data-toggle="collapse"]').each(function(){
+                            var objectID=$(this).attr('href');
+                            if($(objectID).hasClass('in')===false)
+                            {
+                                 $(objectID).collapse('show');
+                            }
+                        });
+                    });
+                    
+                    $('#collapseAll').on('click',function(){
+                        
+                        $('a[data-toggle="collapse"]').each(function(){
+                            var objectID=$(this).attr('href');
+                            $(objectID).collapse('hide');
+                        });
+                    });
+                    
+		});
