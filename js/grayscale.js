096dbfa15e6f00bf1e5a890a190898442d821ecb
djames2127 <djames@nubrink.com>
2016-10-09 15:33:16 +0800

    DTD V1.01

--- js/grayscale.js
+++ js/grayscale.js
@@ -51,17 +51,3 @@ $(function() {
        $(".navbar-fixed-top").addClass("top-nav-collapse");
   });
 });
-
-
-$(function() {
-  $(".expand").on( "click", function() {
-    $(this).next().slideToggle(500);
-    $expand = $(this).find(">:first-child");
-    
-    if($expand.text() == "+") {
-      $expand.text("-");
-    } else {
-      $expand.text("+");
-    }
-  });
-});
